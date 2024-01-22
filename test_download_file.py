79dfd5e7f542d1997b84842090164c93347c3952
Egor Alekhno <alehnoegor@gmail.com>
2024-01-22 21:58:01 +0300

    add changes

---
+++ test_download_file.py
@@ -0,0 +1,33 @@
+import time
+
+import requests
+from selene import query
+from selene.support.shared import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+def test():
+    # options = webdriver.ChromeOptions()
+    # prefs = {
+    #     "download.default_directory": "C:\\Users\\aev32\\PycharmProjects\\QA_GURU_Python_9_7\\tmp",
+    #     "download.prompt_for_download": False
+    # }
+    # options.add_experimental_option("prefs", prefs)
+    #
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    # browser.config.driver = driver
+    #
+    # browser.open("https://github.com/pytest-dev/pytest/blob/main/README.rst")
+    # download_url = browser.element("[data-testid='raw-button']").get(query.attribute("href"))
+    #
+    # # content = requests.get(url=download_url).content
+    #
+    # with open("tmp/readme2.rst", 'wb') as file:
+    #     file.write(content)
+
+    with open("tmp/readme2.rst") as file:
+        a = file.read()
+        assert "test_answer" in a
+    # browser.element("[data-testid='download-raw-button']").click()
+    # time.sleep(5)
