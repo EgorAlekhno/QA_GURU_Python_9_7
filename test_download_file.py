55ef8ad5938ff6d03ae3c23b1ae221b0025516a4
Egor Alekhno <alehnoegor@gmail.com>
2024-01-25 21:53:10 +0300

    Add changes

--- test_download_file.py
+++ test_download_file.py
@@ -7,24 +7,27 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+from script_os import TMP_DIR
+
+
 def test():
-    # options = webdriver.ChromeOptions()
-    # prefs = {
-    #     "download.default_directory": "C:\\Users\\aev32\\PycharmProjects\\QA_GURU_Python_9_7\\tmp",
-    #     "download.prompt_for_download": False
-    # }
-    # options.add_experimental_option("prefs", prefs)
-    #
-    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    # browser.config.driver = driver
-    #
-    # browser.open("https://github.com/pytest-dev/pytest/blob/main/README.rst")
-    # download_url = browser.element("[data-testid='raw-button']").get(query.attribute("href"))
-    #
-    # # content = requests.get(url=download_url).content
-    #
-    # with open("tmp/readme2.rst", 'wb') as file:
-    #     file.write(content)
+    options = webdriver.ChromeOptions()
+    prefs = {
+        "download.default_directory": TMP_DIR,
+        "download.prompt_for_download": False
+    }
+    options.add_experimental_option("prefs", prefs)
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    browser.config.driver = driver
+
+    browser.open("https://github.com/pytest-dev/pytest/blob/main/README.rst")
+    download_url = browser.element("[data-testid='raw-button']").get(query.attribute("href"))
+
+    content = requests.get(url=download_url).content
+
+    with open("tmp/readme2.rst", 'wb') as file:
+        file.write(content)
 
     with open("tmp/readme2.rst") as file:
         a = file.read()
